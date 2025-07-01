d02a1f2705c6024de908e17a05742ac5f38a72b5
IJustKen <168433488+IJustKen@users.noreply.github.com>
2025-07-01 12:25:49 +0530

    Update sort_array_by_parity.py

--- arrays/sort_array_by_parity.py
+++ arrays/sort_array_by_parity.py
@@ -11,14 +11,14 @@ class Solution:
     def sortArrayByParity(self, nums: List[int]) -> List[int]:
         last_even = None
         for i in range(len(nums)):
-            if nums[i]%2 == 0:
+            if nums[i]%2 == 0:        #find first even elem idx
                 last_even = i
                 break
         if last_even == None:
             return nums
         
-        nums[last_even],nums[0] = nums[0],nums[last_even]
-        last_even = 0
+        nums[last_even],nums[0] = nums[0],nums[last_even]        #swap and make it the first elem
+        last_even = 0        #dont forget to update the last_even idx
         for i in range(1,len(nums)):
             if last_even == len(nums)-1:
                 break
