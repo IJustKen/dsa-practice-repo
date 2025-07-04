df8fd2f06403591f6bf8391e6b3277f12329bdf9
IJustKen <168433488+IJustKen@users.noreply.github.com>
2025-07-05 00:18:17 +0530

    Update binary_search.py

--- arrays/binary_search.py
+++ arrays/binary_search.py
@@ -1,1 +1,19 @@
+#basic binary search implemented
+#no recursion just pointers better
 
+class Solution:
+    def search(self, nums: List[int], target: int) -> int:
+        low = 0
+        high = len(nums) -1
+        while low <= high:    #stopping condition 
+            mid = (low+high)//2
+            if nums[mid] == target:  
+                return mid    #stopping condition
+            elif nums[mid] < target:
+                low = mid + 1
+            elif nums[mid] > target:
+                high = mid -1
+
+        return -1    #return if not found
+        
+        
