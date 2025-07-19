23c7a756371246f697bbf905506eddee87c08462
IJustKen <168433488+IJustKen@users.noreply.github.com>
2025-07-19 21:32:29 +0530

    Update validate_binary_search_tree.py

--- binary_trees/validate_binary_search_tree.py
+++ binary_trees/validate_binary_search_tree.py
@@ -31,6 +31,31 @@ class Solution:
                 return False
         return True
 
+
+#RECURSIVE SOLUTION
+
+class Solution:
+    def isValidBST(self, root: Optional[TreeNode]) -> bool:
+
+      #logic is this - every node has a value range basically starting from root, root can be any damn number
+      #so between -inf and +inf. THen the left and right children are restricted to -inf and node.val, node.val and +inf respectively.
+      #and so on you go there will always be a minimum and maximum range.
+      
+        def valid(node,minimum,maximum):      #helper
+            if not node:
+                return True
+            if not (node.val>minimum and node.val<maximum):        #if not in that range means invalid
+                return False
+            
+            return valid(node.left,minimum,node.val) and valid(node.right,node.val,maximum)    #else we see if children are also following BST
+
+        return valid(root, float('-inf'),float('inf'))        #start check from root, since root can be anything range is -inf to +inf
+
+        
+
+
+        
+
         
 
 
