32bad518b30d17773e2db7876f2081d858629673
IJustKen <168433488+IJustKen@users.noreply.github.com>
2026-06-10 22:32:55 +0530

    Update remove_nth_node_from_end_of_list.py

--- linked_list/remove_nth_node_from_end_of_list.py
+++ linked_list/remove_nth_node_from_end_of_list.py
@@ -1,44 +1,30 @@
 # Definition for singly-linked list.
-# class ListNode:
-#     def __init__(self, val=0, next=None):
-#         self.val = val
-#         self.next = next
-
-#Remove the nth node from the end of the list
-
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
 class Solution:
-
-  #my idea is to convert it to nth node from start
-  
     def removeNthFromEnd(self, head: Optional[ListNode], n: int) -> Optional[ListNode]:
-      
-        def chainlen(node):    #helper to get length of the linked list
-            i = 0
-            curr = node
-            while curr:
-                curr = curr.next
-                i += 1
-            return i
-        
-        length = chainlen(head)      #get it
-      
-        if length == 1:
-            return None        #edge case when only one node is there and you remove it
-          
-        from_start = length - n + 1      #conversion to pos from start of linked list
+        steps = 0
+        last = head
+        while last.next:
+            last = last.next
+            steps += 1
+        steps += 1
+
+        prev = None
         curr = head
-        for i in range(0,max(0,from_start-2)):    #get to the node right before the node to be removed
+        steps = steps - n
+        count = 0
+        while count != steps:
+            prev = curr
             curr = curr.next
+            count += 1
 
-        #now curr is the node right before the node to be removed
-          
-        if curr.next:          #just making sure it exists and only then we do this 
-            temp = curr.next      
-            curr.next = curr.next.next      #make it point next to next now
-          
-        if from_start == 1:    #edge case when removed elem is the first node in list
-            return temp
-        return head      #normal case
-
-
-        
+        # delete curr
+        if prev is None:
+            return curr.next
+            
+        prev.next = curr.next
+        curr.next = None
+        return head
