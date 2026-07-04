be2b5f7db2296564c7af11e258c6120782313ef7
IJustKen <168433488+IJustKen@users.noreply.github.com>
2026-07-04 23:44:14 +0530

    Update two_sum_ii.py

--- two_pointers/two_sum_ii.py
+++ two_pointers/two_sum_ii.py
@@ -1,6 +1,17 @@
 #Approach 1: O(n) solution we can try since the array is sorted non decreasing order
-
-
+# True 2 pointers
+class Solution:
+    def twoSum(self, numbers: List[int], target: int) -> List[int]:
+        p1 = 0
+        p2 = len(numbers) -1
+        while p1<p2:
+            curr_sum = numbers[p1] + numbers[p2]    # this was the main overhead, do not keep doing n1+n2 in each if statement, just calculate sum once and reuse
+            if curr_sum > target:
+                p2 -= 1
+            elif curr_sum < target:
+                p1 += 1
+            else:
+                return [p1+1, p2+1]
 
 
 # Approach 2: slow as this is O(nlogn)
