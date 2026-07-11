15477bcf2552e9ec87076f7b09a4d00ca8a952e2
IJustKen <168433488+IJustKen@users.noreply.github.com>
2026-07-11 14:09:53 +0530

    Update lowest_common_ancestor_of_BST.py

--- binary_trees/lowest_common_ancestor_of_BST.py
+++ binary_trees/lowest_common_ancestor_of_BST.py
@@ -5,77 +5,23 @@
 #         self.left = None
 #         self.right = None
 
-#this is the more optimized memory efficient one i wrote afterwards, first sol is beneath this one
 class Solution:
     def lowestCommonAncestor(self, root: 'TreeNode', p: 'TreeNode', q: 'TreeNode') -> 'TreeNode':
-        #logic is we start from root, wherever the path changes for either of the searches we stop and return it as
-        #LCA
-        curr = root
-        flag = True      #flag to mark that the search for p and q is going together
-        while flag:            
-            if curr.val > p.val and curr.val > q.val:        #means both p and q in same direction from here
-                curr = curr.left
-                continue
-            if curr.val < p.val and curr.val < q.val:        #p and q in same direction from here
-                curr = curr.right
-                continue
-            if curr.val == p.val and curr.val == q.val:        #both coincide actually doesnt exist but ok
-                return curr
-            else:                                        #anything else means p and q in different directions which means this must be the LCA rn
-                                                        #so we break and return the curr node
-                flag = False
-        return curr
-#kinda bruteforce ish this one fine the path for both then go backwards and see where they first meet
-class Solution:
-    def lowestCommonAncestor(self, root: 'TreeNode', p: 'TreeNode', q: 'TreeNode') -> 'TreeNode':
-        path1 = []
-        path2 = []
-        curr = root
-        while curr:      #find for p
-            if not curr:
-                break
-            if curr.val == p.val:
-                path1.append(p)
-                break
-            elif curr.val>p.val:
-                path1.append(curr)
-                curr = curr.left
-                continue
-            elif curr.val<p.val:
-                path1.append(curr)
-                curr = curr.right
-                continue
-        
-        curr = root
-        while curr:            #find for q
-            if not curr:
-                break
-            if curr.val == q.val:
-                path2.append(q)
-                break
-            elif curr.val>q.val:
-                path2.append(curr)
-                curr = curr.left
-                continue
-            elif curr.val<q.val:
-                path2.append(curr)
-                curr = curr.right
-                continue
+        def dfs(node, p, q):
+            if not node:    # base case
+                return None
                 
-        p1,p2 = len(path1)-1,len(path2)-1
-
-        while path1[p1].val != path2[p2].val and p1 >= 0 and p2 >= 0:    #then see their last common ancestor
-            if p1>p2:
-                p1 -= 1
-            elif p1<p2:
-                p2 -= 1
-            else:
-                p1 -= 1
-                p2 -= 1
+            if not ((p.val<node.val and q.val < node.val) or (p.val>node.val and q.val > node.val)):
+                # if p is on one side and q is on the other, it means this is the point where they branch out, and this node
+                # is the LCA
+                return node    # return it hence
+                
+            if p.val < node.val:    # both on left, search left
+                return dfs(node.left, p, q)
+            elif p.val > node.val:    # both on right, search right
+                return dfs(node.right, p, q)
         
-        return path1[p1]
-        
-
+        return dfs(root, p, q)    # call the function from root
 
 
         
