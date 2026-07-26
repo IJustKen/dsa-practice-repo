7b74b389efaeb1c52351f95db31d3a5c3d044927
IJustKen <168433488+IJustKen@users.noreply.github.com>
2026-07-27 00:16:12 +0530

    Update combination_sum_ii.py

--- recursion_backtracking/combination_sum_ii.py
+++ recursion_backtracking/combination_sum_ii.py
@@ -1,35 +1,24 @@
-#same as combination sum but duplicates are present
-
 class Solution:
-
-  #we will do this like SUBSETS II, same kinda logic and ofc Combination Sum I
-  
     def combinationSum2(self, candidates: List[int], target: int) -> List[List[int]]:
         res = []
-        candidates = sorted(candidates)      #sort to deal with duplicates properly
-        combo = []  
 
-        def dfs(i,s):      #s is the sum of the curr combo, kept for more efficiency in argument rather than doing sum(combo)
-            if s > target:    #prune
+        candidates.sort()  # so that we can try solving it similar way as combination sum I 
+
+        def dfs(i, temp, total):
+            if total == target:  # base case
+                res.append(temp.copy())
                 return
-            if i >= len(candidates):    #check n prune
-                if s == target:
-                    res.append(combo.copy())
-                return
-            if s == target:    #append to result
-                res.append(combo.copy())
+            if total > target or i >= len(candidates):  # base case, end the tree
                 return
             
-            combo.append(candidates[i])    #normal case we include the curr number
-            s += candidates[i]
-            dfs(i+1,s)
-            combo.pop()    #backtrack
-            s -= candidates[i]
-            while i+1 < len(candidates) and candidates[i+1] == candidates[i]:
-                i += 1
-            dfs(i+1,s)    #case to include the next DIFFERENT number, if you get the same you will get duplicate ans
+            temp.append(candidates[i])  # include the current element 
+            dfs(i+1, temp, total + candidates[i])  # include a potential duplicate of the current number eg - [1,1,2,4,5,5], 1 just appended, now this path will 
+            # append next 1
+            temp.pop()  # once you explore that path, remove the appended element
 
-        dfs(0,0)
-        return res 
+            while i+1 < len(candidates) and candidates[i+1] == candidates[i]:  # now next path to explore, we do not include the duplicate at all
+                i += 1  # thus skip until you get a new element
+            dfs(i+1, temp, total)  # explore that path now
 
-        
+        dfs(0, [], 0)
+        return res
