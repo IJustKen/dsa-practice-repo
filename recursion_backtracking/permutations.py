89426ef1f41f7af082bf774a2d8ef05779774e34
IJustKen <168433488+IJustKen@users.noreply.github.com>
2026-08-01 02:05:21 +0530

    Update permutations.py

--- recursion_backtracking/permutations.py
+++ recursion_backtracking/permutations.py
@@ -1,5 +1,30 @@
 #Given an array nums of distinct integers, return all the possible permutations. You can return the answer in any order.
 
+# OPTIMAL SOLUTION
+
+class Solution:
+    def permute(self, nums: List[int]) -> List[List[int]]:
+        n = len(nums)
+      
+        def dfs(temp):
+            if len(temp) == 0:  # for an empty array the only permutation possible is [[]]
+                return [[]]
+              
+            perms = dfs(temp[1:])  # otherwise get the list of permutations of all elements but the first one
+            res = []
+
+            for perm in perms:
+                for i in range(len(perm)+1):
+                    perm_copy = perm.copy()
+                    perm_copy.insert(i, temp[0])  # later we insert the first element of the array at all possible positions, including last, hence len()+1 not len()
+                    res.append(perm_copy)
+            return res
+        
+        return dfs(nums)
+            
+                
+# HERE IS A SLOWER SOLUTION
+
 class Solution:
 
   #similar logic to subsets ques, there at each step we decided whether to include the curr index or not
