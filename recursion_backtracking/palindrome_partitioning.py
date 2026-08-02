a6a6a3871a8c1ea930b187598acd472b133e9517
IJustKen <168433488+IJustKen@users.noreply.github.com>
2026-08-03 00:21:56 +0530

    Update palindrome_partitioning.py

--- recursion_backtracking/palindrome_partitioning.py
+++ recursion_backtracking/palindrome_partitioning.py
@@ -1,6 +1,42 @@
 #Given a string s, partition s such that every substring of the partition is a palindrome. 
 #Return all possible palindrome partitioning of s.
 
+class Solution:
+    def partition(self, s: str) -> List[List[str]]:
+        res = []
+
+        def isPali(text):
+            ans = True
+            for i in range(len(text)//2):
+                ans = ans and (text[i] == text[len(text)-i-1])
+            return ans
+        
+        def dfs(pali: list, temp: list, idx: int):
+            nonlocal res
+            if idx >= len(s):
+                if isPali(pali):
+                    temp.append("".join(pali.copy()))
+                    res.append(temp.copy())
+                    temp.pop()
+                return
+
+            if not isPali(pali):
+                pali.append(s[idx])
+                dfs(pali, temp, idx+1)
+                pali.pop()
+                return
+
+            if pali:
+                temp.append("".join(pali.copy()))
+                dfs([s[idx]], temp, idx+1)
+                temp.pop()
+
+            pali.append(s[idx])
+            dfs(pali, temp, idx+1)
+        
+        dfs([], [], 0)
+        return res
+
 
 class Solution:
 
