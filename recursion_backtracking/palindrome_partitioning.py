38925601c9519a66e2fff6b53c8ddc74abf1292f
IJustKen <168433488+IJustKen@users.noreply.github.com>
2026-08-03 00:36:00 +0530

    Update palindrome_partitioning.py

--- recursion_backtracking/palindrome_partitioning.py
+++ recursion_backtracking/palindrome_partitioning.py
@@ -43,35 +43,37 @@ class Solution:
         return res
 
 
-class Solution:
+
 
   #well so if you look at the recursion tree it is like this
   #start from say string aab - you can make 3 parts a, aa or aab. Now aab is not pali so prune it
   #a and aa are pali so continue. Now for 'a' the remaining letters are ab - so furhther branches 'a' and 'ab' of which ab is
   #not pali so prune. 'a' is pali. Now remaining 'b' only which is also pali. This entire brancg gave us 'a','a','b' see.
   #now recall the 'aa' earlier - remaining only 'b' which is pali so 'aa','b' also allowed.
-
-  
+class Solution:
     def partition(self, s: str) -> List[List[str]]:
         res = []
-        part = []      #to append the palis alog one partitioning style
+        part = []      
 
-        def isPali(sub):      #helper function to check if palindrome
+        def isPali(sub):  
+            # helper to check palindrome or not
             return sub == sub[::-1]
 
         def dfs(i):
             nonlocal res
             nonlocal part
-            if i>=len(s):      #i is the idx of the letter we are starting from to check pali
-                res.append(part.copy())    #base case when all letters are done, we must've reached the end and obtained a partition
+            # base case exit after appending to res
+            if i>=len(s):     
+                res.append(part.copy())    
                 return
               
-            for j in range(i,len(s)):    #else, like explained in sol, we keep trying substrings of increasing lengths
-                sub = s[i:j]                          #starting from i going all the way to len(s)
+            for j in range(i+1,len(s)+1): 
+                # index starts from i till j, j increases one by one
+                sub = s[i:j]    # possible substring that could be palindrome
                 if isPali(sub):
-                    part.append(sub)      #append the curr pali
-                    dfs(j+1)                    #do dfs and look for this particular partitioning style
-                    part.pop()            #remove the pali you just found to look for other way of partitioning (diff pali)
+                    part.append(sub)      # partition here
+                    dfs(j)                    
+                    part.pop()    # do not partition here, check next bigger substring now
         
         dfs(0)
         return res
